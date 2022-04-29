e76214d13264cde5171d91b9d4f7a79349fc6c24
abel bazan <abelbazan51@gmail.com>
2022-04-28 21:10:00 -0300

    javascript

---
+++ Desktop/javascript/js/main.js
@@ -0,0 +1,43 @@
+//esto es un comentario //
+/*comentario en varias lineas */
+//ES6
+//let numeroUno;
+
+//numeroUno = 1;
+//numeroUno = "56";
+
+//let nombre = "maxi";
+
+//const PI = 3.141516;
+
+//let nombreUsuario = prompt("ingresa tu nombre");
+
+//alert("hola " + nombreUsuario);
+
+//console.log("hola " + nombreUsuario);
+
+let nota = prompt("cuanto es 3+3");
+
+console.log(nota >= 6);
+
+if (nota == 6) {
+  console.log("me diste");
+} else {
+  console.log("agua");
+}
+
+let disparo = prompt("cuanto es 2x2");
+console.log(nota >= 4);
+if (disparo == 4) {
+  console.log("correcto");
+} else {
+  console.log("equibocado");
+}
+
+let numero = prompt("cuanto es 9+9");
+console.log(nota <= 18);
+if (numero == 18) {
+  console.log("correcto");
+} else {
+  console.log("incorrecto");
+}
